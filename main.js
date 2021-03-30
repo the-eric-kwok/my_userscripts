b20264e87b7b741334ad88457e6146a4a972c850
Eric Kwok <erickwok404@gmail.com>
2021-03-30 17:59:09 +0800

    囿于 GreasyFork 政策，将 GM_config 库换回了官方的链接，大伙儿凑合凑合用吧

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         [Reload]智慧树共享课刷课,智慧树共享课自动跳过题目，智慧树共享课自动播放下一个视频，智慧树共享课自动播放未完成的视频
 // @namespace    https://github.com/the-eric-kwok/zhihuishu_reload
-// @version      1.1.1
+// @version      1.1.2
 // @description  智慧树共享课刷课,智慧树共享课自动跳过题目，智慧树共享课自动播放下一个视频，智慧树共享课自动播放未完成的视频
 // @author       EricKwok, C选项_沉默
 // @homepage     https://github.com/the-eric-kwok/zhihuishu_reload
 // @supportURL   https://github.com/the-eric-kwok/zhihuishu_reload/issues
 // @match        *://studyh5.zhihuishu.com/videoStudy*
 // @match        *://onlineexamh5new.zhihuishu.com/stuExamWeb.html*
-// @require      https://codechina.csdn.net/-/snippets/198/raw/master/GM_config.js
+// @require      https://greasyfork.org/scripts/28536-gm-config/code/GM_config.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_deleteValue

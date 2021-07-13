b32d11508b994ba8fdcaafac44dd2985e2a97759
the-eric-kwok <erickwok404@gmail.com>
2021-07-13 15:48:14 +0800

    修复了点击复制时跳转到网页末尾的问题

--- No_GM_config.js
+++ No_GM_config.js
@@ -1,13 +1,16 @@
-/*
- * @name: [Reload]智慧树共享课挂机刷课助手
- * @version: 1.2.4
- * @description: 智慧树共享课刷课、跳过弹题、自动换集、自动1.5倍速、自动静音、自动标清、解除考试复制封印及一键复制题目到剪贴板
- * @author: EricKwok, C选项_沉默
- * @include: studyh5.zhihuishu.com,onlineexamh5new.zhihuishu.com
- * @createTime: 2021-7-5 12:00
- * @updateTime: 2021-7-11 12:40
- * @license: GPL
- */
+// ==UserScript==
+// @name         [Reload]智慧树共享课挂机刷课助手（无配置界面）
+// @namespace    https://github.com/the-eric-kwok/zhihuishu_reload
+// @version      1.0.0
+// @description  智慧树共享课刷课、跳过弹题、自动换集、自动1.5倍速、自动静音、自动标清、解除考试复制封印及一键复制题目到剪贴板
+// @author       EricKwok, C选项_沉默
+// @homepage     https://github.com/the-eric-kwok/zhihuishu_reload
+// @supportURL   https://github.com/the-eric-kwok/zhihuishu_reload/issues
+// @match        *://studyh5.zhihuishu.com/videoStudy*
+// @match        *://onlineexamh5new.zhihuishu.com/stuExamWeb.html*
+// @run-at       document-end
+// @license      GPL
+// ==/UserScript==
 
 var gxkEnable = true;
 var copyEnable = true;
@@ -308,17 +311,17 @@ function copyEnabler() {
 function autoCopy() {
     function _autoCopy() {
         console.log($(this).text());
-        let transfer = document.createElement('input');
-        document.body.appendChild(transfer);  // FIXME: 修复点击时跳转到网页末尾的问题
-        transfer.value = $(this).text();  // 这里表示想要复制的内容
-        transfer.focus();
-        transfer.select();
+        let tmpInput = document.createElement('input');
+        $(this).append(tmpInput)
+        tmpInput.value = $(this).text();  // 这里表示想要复制的内容
+        tmpInput.focus();
+        tmpInput.select();
         if (document.execCommand('copy')) {
             document.execCommand('copy');
         }
-        transfer.blur();
+        tmpInput.blur();
         console.log('复制成功');
-        document.body.removeChild(transfer);
+        $(tmpInput).remove();
         showDialog("复制成功！", 1, true, true);
         $(this).css("background-color", "#ECECEC");
         setTimeout(function (elem) {

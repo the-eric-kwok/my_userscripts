2c77c6cfb0e842142c3fa3f021f960cbc64422b7
EricKwok <16463312+the-eric-kwok@users.noreply.github.com>
2021-07-13 16:16:22 +0800

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Reload]智慧树共享课挂机刷课助手
 // @namespace    https://github.com/the-eric-kwok/zhihuishu_reload
-// @version      1.2.4
+// @version      1.2.5
 // @description  智慧树共享课刷课、跳过弹题、自动换集、自动1.5倍速、自动静音、自动标清、解除考试复制封印及一键复制题目到剪贴板
 // @author       EricKwok, C选项_沉默
 // @homepage     https://github.com/the-eric-kwok/zhihuishu_reload
@@ -457,7 +457,7 @@ function autoCopy() {
         console.log($(this).text());
         let tmpInput = document.createElement('input');
         $(this).append(tmpInput)
-        tmpInput.value = $(this).text();  // 这里表示想要复制的内容
+        tmpInput.value = $(this).text();
         tmpInput.focus();
         tmpInput.select();
         if (document.execCommand('copy')) {

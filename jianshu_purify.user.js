ba21e2f273f30a4d6ff59b0d4e3a202714236d85
EricKwok <16463312+the-eric-kwok@users.noreply.github.com>
2023-04-10 19:23:10 +0800

    更新简书净化脚本

--- jianshu_purify.user.js
+++ jianshu_purify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         简书净化
 // @namespace    http://github.com/the-eric-kwok/
-// @version      0.1
+// @version      0.2
 // @description  简书移除热门故事、广告
 // @author       EricKwok
 // @match        *://www.jianshu.com/p/*
@@ -10,19 +10,22 @@
 
 (function() {
     'use strict';
-    setInterval(() => {
-        document.querySelectorAll('._1Prj5h').forEach(e=>{
+
+    let setNewTask = (ms) => setTimeout(() => {
+        let ads = [
+            ...document.querySelectorAll('._1Prj5h'),
+            ...document.querySelectorAll('._2OwGUo'),
+            ...document.querySelectorAll('.adad_container'),
+            ...document.querySelectorAll('._11TSfs'),
+            ...document.querySelectorAll("body > div:nth-child(15)"),
+            ...document.querySelectorAll("body > div:nth-child(16)"),
+            ...document.querySelectorAll("body > div:nth-child(17)"),
+            ...document.querySelectorAll("body > div:nth-child(18)"),
+        ];
+        ads.forEach(e=>{
             e.remove();
         });
-        document.querySelectorAll('._2OwGUo').forEach(e=>{
-            e.remove();
-        });
-        document.querySelectorAll('.adad_container').forEach(e=>{
-            e.remove();
-        });
-        document.querySelectorAll('._11TSfs').forEach(e=>{
-            e.remove();
-        });
-    }, 1000);
-    // Your code here...
+		    setNewTask(ads.length > 0 ? 100 : 1000);
+    }, ms);
+	  setNewTask(100);
 })();
